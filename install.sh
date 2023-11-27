9de1f49e8239076802ce351333aa501be74c7155
Janis Hutz <development@janishutz.com>
2023-11-27 21:18:16 +0100

    fix

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ mount "/dev/${driveName}2" /mnt
 mkdir /mnt/boot
 mount "/dev/${driveName}1" /mnt/boot
 
-pacstrap -c -K /mnt base linux-zen linux-firmware nano networkmanager efibootmgr grub man git xfce4 base-devel fish sudo gdm plymouth --noconfirm
+pacstrap -K /mnt base linux-zen linux-firmware nano networkmanager efibootmgr grub man git xfce4 base-devel fish sudo gdm plymouth --noconfirm
 
 
 echo "
